26060873ebb816ad9bea80dcdae8e03bd78e5689
Blackdrg <daredevil0101a@gmail.com>
2026-07-09 00:57:18 +0530

    fix: remove unused customer-mobile mock files
    - Remove __mocks__/expo-image.js (not referenced in jest config or tests)
    - Remove __mocks__/react-native-root-toast.js (not referenced in jest config or tests)

--- apps/customer-mobile/__mocks__/expo-image.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { View, Image as RNImage, ImageProps as RNImageProps } from 'react-native';
-
-const Image = (props: RNImageProps) => {
-  return <RNImage {...props} />;
-};
-
-export default Image;
--- apps/customer-mobile/__mocks__/react-native-root-toast.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-export default function Toast() {
-  return null;
-}
-export const show = jest.fn();
-export const hide = jest.fn();
